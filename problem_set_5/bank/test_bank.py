deb09e753d35d6ad9a8dffd3b9195e447af87d15
Austin Zehnder <134451398+austinzehnder@users.noreply.github.com>
2023-12-19 11:15:32 -0800

    Create test_bank.py

---
+++ problem_set_5/bank/test_bank.py
@@ -0,0 +1,19 @@
+from bank import value
+
+def test_hello():
+    assert value("hello") == 0
+
+def test_hello_upper():
+    assert value("HELLO") == 0
+
+def test_h_preceding():
+    assert value("hard") == 20
+
+def test_h_preceding_upper():
+    assert value("HARD") == 20
+
+def test_else():
+    assert value("abc") == 100
+
+def test_else_upper():
+    assert value("DEF") == 100
